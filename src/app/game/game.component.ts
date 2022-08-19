305cc4f045bfb93c47f8cddd8de35ee047497b52
Daniel Johannsen <daniel-johannsen@web.de>
2022-08-19 18:18:29 +0200

    firebase functions

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -3,35 +3,44 @@ import { Game } from 'src/models/game';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent implements OnInit {
-
-  newCardAnimation = false;
-  currentCard: string = '';
+export class GameComponent implements OnInit {  
   game: Game;
+  gameId: string;
 
-  constructor(private firestore: AngularFirestore, public dialog: MatDialog) { }
+  constructor(private route: ActivatedRoute, private firestore: AngularFirestore, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.newGame();
-    this.firestore
-    .collection('games')
-    .valueChanges()
-    .subscribe((game) => {
-      console.log('Game update', game);
+    this.route.params.subscribe((params) => {
+      console.log(params['id']);
+      this.gameId = params['id'];
+      this
+      .firestore
+      .collection('games')
+      .doc(this.gameId)
+      .valueChanges()
+      .subscribe((game: any) => {
+        console.log('Game update', game);
+        this.game.currentPlayer = game.currentPlayer;
+        this.game.playedCards = game.playedCards;
+        this.game.players = game.players;
+        this.game.stack = game.stack;
+        this.game.newCardAnimation = game.newCardAnimation;
+        this.game.currentCard = game.currentCard;
+      });
     });
   }
 
   newGame() {
     this.game = new Game();
-    this.firestore
-    .collection('games')
-    .add(this.game.toJson());
+    
   }
 
   checkPlayerArray() {
@@ -47,17 +56,24 @@ export class GameComponent implements OnInit {
       alert('The game is over');
       window.open("http://daniel-johannsen.developerakademie.net/ring-of-fire/index.html", "_self");
     }
-    if (!this.newCardAnimation) {
-      this.currentCard = this.game.stack.pop();
-      this.newCardAnimation = true;
-      this.game.currentPlayer++;
-      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+    if (!this.game.newCardAnimation) {
+      this.takeCard();
 
       setTimeout(() => {
-        this.game.playedCards.push(this.currentCard);
-        this.newCardAnimation = false;
+        this.game.playedCards.push(this.game.currentCard);
+        this.game.newCardAnimation = false;
+        this.saveGame();
       }, 1000);
     }
+  }
+
+
+  takeCard() {
+    this.game.currentCard = this.game.stack.pop();
+    this.game.newCardAnimation = true;
+      this.game.currentPlayer++;
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+      this.saveGame();
   }
 
   
@@ -67,8 +83,17 @@ export class GameComponent implements OnInit {
     dialogRef.afterClosed().subscribe((name: string) => {
       if (name && name.length > 0) {
         this.game.players.push(name);
+        this.saveGame();
       }
     });
+  }
+
+  saveGame() {
+    this
+    .firestore
+    .collection('games')
+    .doc(this.gameId)
+    .update(this.game.toJson());
   }
 
 }
